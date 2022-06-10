a911a5671d4de0c890ea7b9832951e93a478439e
Guilherme Reis <83259375+gui1hermereis@users.noreply.github.com>
2022-06-10 02:04:11 -0300

    Delete src directory

--- src/sql/sair.php
+++
@@ -1,6 +0,0 @@
-<?php
-    //Desconecta quem esta logado por um botão e o redireciona
-    session_start();
-    session_destroy();//Destroi a sessão
-    header("Location: ../paginas/acessarconta.php");//Redirecionamento
-?>
