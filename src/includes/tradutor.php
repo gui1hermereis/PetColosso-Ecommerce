ec0ebd23b57ce37884f95ee95e41fd9fcd3fc8b6
Guilherme Reis <83259375+gui1hermereis@users.noreply.github.com>
2022-06-19 20:41:47 -0300

    Delete src directory

--- src/includes/tradutor.php
+++
@@ -1,8 +0,0 @@
-<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-
-<!--Codigo JavaScript-->
-<script type="text/javascript">
-    function googleTranslateElementInit() {
-        new google.translate.TranslateElement({pageLanguage: 'pt-br'}, 'google_translate_element');
-    }
-</script>    
